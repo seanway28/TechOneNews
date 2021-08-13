d3043126b2fd59c34669251267ef4777f87d340f
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-13 17:55:51 -0400

    post routes api

---
+++ routes/post-routes.js
@@ -0,0 +1,84 @@
+const sequelize = require('../../config/connection');
+const router = require('express').Router();
+const { Post, User, Vote, Comment } = require('../../models');
+
+// Get All Posts
+
+outer.get('/', (req, res) => {
+    console.log('=======================');
+    Post.findAll({
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
+        order: [[ 'created_at', 'DESC']],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+    }).then(dpPostData => res.json(dpPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    });
+
+ //Get a single post
+ router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+          if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id.' });
+            return;
+          }
+          res.json(dbPostData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  }); 
+
+  // Create a Post
+  router.post('/', (req, res) => {
+      // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+      Post.create({
+          title: req.body.title,
+          post_url: req.body.post_url,
+          user_id: req.body.user_id
+      })
+        .then(dbPostData => res).json(dbPostData)
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json(err);
+        });
+  });
+
+  // API/Posts/Upvotes
+  router.put('/upvote', (req, res) =>)
