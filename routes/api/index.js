532bb329ef3dd1cc3dfd49fb1341522ca360d53f
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-17 12:46:11 -0400

    changes made to index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+//const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
@@ -8,4 +8,16 @@ router.use('/users', useRoutes);
 router.use('posts', post-routes);
 router.use('/comments', commentRoutes);
 
-module.exports = router;
+//module.exports = router;
+////////////////////////////////////////////////////////////////////
+
+const router = require('express').router();
+const apiRoutes = require('./api');
+
+router.apiRoutes('/api, apiRoutes');
+
+router,use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = routes;
