d987efce28f109cf793dda0ae3d1bbac5ebffbd5
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-18 13:02:29 -0400

    test new route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes/');
-const sequelize = require('../../config/connection');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,3 +16,4 @@ sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 
 });
+
