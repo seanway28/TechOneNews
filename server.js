edef1176e62d958afa2d35f9aa8637310a5b3212
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-08-19 07:17:36 -0400

    handlebars server

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const routes = require('./routes/');
 const sequelize = require('./config/connection');
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils"}));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Turn Routes ON
 app.use(routes);
